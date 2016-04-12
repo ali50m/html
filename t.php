e3204a50fd0f0bacfc1d945f3a06cb915dc19e5c
wliu <ali50m@live.com>
2016-04-12 16:26:04 +0800

    add php get code

--- t.php
+++ t.php
@@ -5,7 +5,13 @@
 	<title>php test</title>
 </head>
 <body>
-	<form action="welcome.php" method="post">
+	<form action="welcome_post.php" method="post">
+		Name: <input type="text" name="name"><br>
+		E-mail: <input type="text" name="email"><br>
+		<input type="submit">
+	</form>
+
+	<form action="welcome_get.php" method="get">
 		Name: <input type="text" name="name"><br>
 		E-mail: <input type="text" name="email"><br>
 		<input type="submit">
---
+++ welcome_get.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>answer</title>
+</head>
+<body>
+	Welcome <?php echo $_GET["name"]; ?><br>
+	Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
