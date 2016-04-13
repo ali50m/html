1d06b150e3b4c422c37dff505fa7a378edd320e7
wliu <ali50m@live.com>
2016-04-13 12:24:14 +0800

    修改了按钮的文字

--- t.php
+++ t.php
@@ -14,7 +14,7 @@
 	<form action="welcome_get.php" method="get">
 		Name: <input type="text" name="name"><br>
 		E-mail: <input type="text" name="email"><br>
-		<input type="submit">
+		<input type="submit" title="123">
 	</form>
 </body>
 </html>
