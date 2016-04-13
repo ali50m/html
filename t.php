92c335fbd4a2cf3dabe4be9b63397d6c2f76c639
wliu <ali50m@live.com>
2016-04-13 12:32:06 +0800

    增加了发送电子邮件模块

--- t.php
+++ t.php
@@ -16,5 +16,23 @@
 		E-mail: <input type="password" name="email"><br>
 		<input type="submit" value="按此提交">
 	</form>
+
+	<!-- 发送电子邮件 -->
+	<form action="MAILTO:ali50m@live.com" method="post" enctype="text/plain">
+
+		<h3>这个表单会把电子邮件发送到 W3School。</h3>
+		姓名：<br />
+		<input type="text" name="name" value="yourname" size="20">
+		<br />
+		电邮：<br />
+		<input type="text" name="mail" value="yourmail" size="20">
+		<br />
+		内容：<br />
+		<input type="text" name="comment" value="yourcomment" size="40">
+		<br /><br />
+		<input type="submit" value="发送">
+		<input type="reset" value="重置">
+
+	</form>
 </body>
 </html>
