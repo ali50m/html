3965278450642834f6524e6e57a73bdded9af641
ali50m-dell <ali50m@live.com>
2016-04-15 10:08:18 +0800

    php3

--- p.php
+++ p.php
@@ -6,7 +6,9 @@
 </head>
 <body>
 <?php 
-echo "hello world\n";
+$request = "https://api.64clouds.com/v1/getServiceInfo?veid=109659&api_key=private_WId4nkZbYJisEjSoFQdpnvq0";
+$serviceInfo = json_decode (file_get_contents ($request));
+print_r ($serviceInfo);
  ?>
 </body>
 </html>
